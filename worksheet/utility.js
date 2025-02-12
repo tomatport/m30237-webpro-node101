4c241d8574046a2101bbc309e76574c8b47fa043
Thomas Robinson <git@tomr.me>
2025-02-12 12:01:47 +0000

    done!

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,40 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
+}
+
+// Create a function called compare that accepts two arrays of numbers and compares the contents.
+export function compare(a, b) {
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
+}
+
+// Create and export a function called largest that accepts an array of numbers and returns the largest.
+export function largest(numberArray) {
+  return Math.max(...numberArray);
+}
+
+// Create and export a function called zeroest that accepts an array of integer numbers and returns the one closest to zero
+export function zeroest(numberArray) {
+  const closest = {
+    number: null,
+    numberAbs: null,
+  };
+
+  for (const number of numberArray) {
+    const numberAbs = Math.abs(number);
+    if (numberAbs < closest.numberAbs || closest.numberAbs === null) {
+      closest.numberAbs = numberAbs;
+      closest.number = number;
+    }
+  }
+
+  return closest.number;
 }
